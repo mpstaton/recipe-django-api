6ae397e4af2ae6a157f0c066acebd618756aebfa
Michael Staton <mpstaton@gmail.com>
2018-12-30 15:19:25 -0800

    created tests and app for users

---
+++ app/user/views.py
@@ -0,0 +1,9 @@
+# Create your views here.
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    serializer_class = UserSerializer
