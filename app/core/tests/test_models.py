188700aa5a10ee4fc51b02fdfcb9af8dd420c4fc
Michael Staton <mpstaton@gmail.com>
2018-12-30 18:25:47 -0800

    added create tags feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,6 +8,7 @@ def sample_user(email='test@londonappdev.com', password='testpass'):
     """Create a sample user"""
     return get_user_model().objects.create_user(email, password)
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
